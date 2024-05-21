39980bbbfbd52a3d3b51bf8644282ed559f2421b
jword <didehdrb21@naver.com>
2024-05-21 17:19:49 +0900

    Server: Request Reader modified

---
+++ Server/src/main/java/org/example/Request.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Map;
+
+@Data
+@NoArgsConstructor
+public class Request {
+    private String method;
+    private String uri;
+    private String protocol;
+    private Map<String, String> headerMap;
+    private byte[] bodyBytes;
+}
--- Server/src/main/java/org/example/RequestReader.java
+++ Server/src/main/java/org/example/RequestReader.java
@@ -12,73 +12,49 @@ import java.util.Map;
 @AllArgsConstructor
 public class RequestReader {
     private final Socket clientSocket;
-    public RequestMessage readRequest() {
+    public Request readRequest() {
         try {
             System.out.println("Reading Request...[ \n");
             DataInputStream dataInputStream = new DataInputStream(clientSocket.getInputStream());
 
-            int packetLength = dataInputStream.readInt();
-            byte[] httpPacket = new byte[packetLength];
-            dataInputStream.readFully(httpPacket);
-            System.out.println("read packet Length: " + packetLength + "\n");
-            System.out.println("httpPacket: " + Arrays.toString(httpPacket) + "\n");
+            // read Request packet
+            StringBuilder requestBuilder = new StringBuilder();
+            String line;
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(dataInputStream));
 
-            String httpRequest = new String(httpPacket);
-            System.out.println("http Packet to String: \n" + httpRequest + "\n");
-
-            System.out.println("start to read request line : {\n" );
-            BufferedReader reader = new BufferedReader(new StringReader(httpRequest));
-            // Request Parsing
-            String line = reader.readLine();
-            System.out.println("read request line: " + line + "\n");
-            String[] requestLine = line.split(" ");
-            String method = requestLine[0];
-            String uri = requestLine[1];
-            String httpVersion = requestLine[2];
-            System.out.println("requestLine: " + Arrays.toString(requestLine) + "\b");
-            System.out.println("method, uri, httpVersion: " + method + uri + httpVersion + "\n");
-            System.out.println("}\n all request line read!!\n");
-
-            // Header Parsing
-            System.out.println("start to read header: {\n" );
+            Request request = new Request();
             Map<String, String> headerMap = new HashMap<>();
-            StringBuilder body = new StringBuilder();
-            boolean isBody = false;
-            while ((line = reader.readLine()) != null) {
-                System.out.println(" read line: " + line + "\n");
-                if (line.isEmpty()) {
-                    System.out.println("read Body!!");
-                    isBody = true;
-                    continue;
-                }
 
-                if (isBody) {
-                    body.append(line);
+            while (!(line = bufferedReader.readLine()).isEmpty()) {
+                int pivot = line.indexOf(": ");
+                if (pivot == -1) {
+                    // read Request Line
+                    String[] requestLine = line.split(" ");
+                    String method = requestLine[0];
+                    String uri = requestLine[1];
+                    String protocol = requestLine[2];
+
+                    request.setMethod(method);
+                    request.setUri(uri);
+                    request.setProtocol(protocol);
                 } else {
-                    int idx = line.indexOf(": ");
-                    if (idx != -1) {
-                        String key = line.substring(0, idx).trim();
-                        String value = line.substring(idx + 1).trim();
-                        headerMap.put(key, value);
-                    }
-
+                    String fieldKey = line.substring(0, pivot).trim().toLowerCase();
+                    String fieldValue = line.substring(pivot + 1).trim();
+                    headerMap.put(fieldKey, fieldValue);
                 }
-
             }
-            System.out.println("}\n all header read! \n");
+            request.setHeaderMap(headerMap);
 
-            for (String key : headerMap.keySet()) {
-                System.out.println("Key: " + key + ", Value: " + headerMap.get(key));
+            if (headerMap.containsKey("content-length")) {
+                System.out.println("content-length: " + headerMap.get("content-length"));
+                byte[] bodyBytes = new byte[Integer.parseInt(headerMap.get("content-length"))];
+                dataInputStream.readFully(bodyBytes);
+                System.out.println("body: " + Arrays.toString(bodyBytes));
+
+                request.setBodyBytes(bodyBytes);
             }
 
-            // Body Parsing
-            System.out.println("body: " + body);
-            System.out.println("start to read body: {\n" );
-            ObjectMapper objectMapper = new ObjectMapper();
-
-            System.out.println("]return request!! \n");
-
-            return  new RequestMessage(method, uri, httpVersion, headerMap, null);
+            return request;
 
         } catch (IOException e) {
             throw new RuntimeException(e);
